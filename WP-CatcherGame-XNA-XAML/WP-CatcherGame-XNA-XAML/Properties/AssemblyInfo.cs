3a55379d52eafaaeb762aad2639c6b9e41a83ccc
yi-cheng-kuo <v6610688@gmail.com>
2014-01-10 18:18:36 +0800

    1-10 把預設語言改成英文 取消掉不必要的硬體支援
    1.把預設語言改成英文
    2.取消掉不必要的硬體支援

--- WP-CatcherGame-XNA-XAML/WP-CatcherGame-XNA-XAML/Properties/AssemblyInfo.cs
+++ WP-CatcherGame-XNA-XAML/WP-CatcherGame-XNA-XAML/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將修訂和組建編號 
 // 指定為預設值:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("zh-TW")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
