6346775fcd25177565b62bfbe16cf4d58959a1e6
yi-cheng-kuo <v6610688@gmail.com>
2014-01-14 16:23:42 +0800

    1-14 在MainPage加入處理6.5.1的程式
    1.開啟別的音樂,然後進入應用程式看有無彈出對話框,確定就會繼續往下,否則關閉
    =>Eixt()仍有Bug要在處理

--- WP-CatcherGame-XNA-XAML/WP-CatcherGame-XNA-XAML/MainPage.xaml.cs
+++ WP-CatcherGame-XNA-XAML/WP-CatcherGame-XNA-XAML/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Windows;
+using System.ComponentModel;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -10,6 +11,9 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+//處理6.5.1
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Media;
 
 namespace WP_CatcherGame_XNA_XAML
 {
@@ -22,17 +26,42 @@ namespace WP_CatcherGame_XNA_XAML
            
             // Can only access the NavigationService when the page has been loaded 
             this.Loaded += new RoutedEventHandler(NavigationPage_Loaded);
-           
+            
         }
         void NavigationPage_Loaded(object sender, RoutedEventArgs e)
         {
-            logo.Begin();
+            //處理6.5.1
+            if (MediaPlayer.State == MediaState.Playing && !MediaPlayer.GameHasControl)
+            {
+                string msg = String.Format("Music + Videos Hub is currently playing music. Catcher Game needs to stop music.\nPlease click Ok to continue or Cancel to close the Catcher Game.");
+                MessageBoxResult res = MessageBox.Show(msg, "Music + Videos Hub", MessageBoxButton.OKCancel);
+                if (res == MessageBoxResult.Cancel)
+                {
+                    Exit();
+                }
+                else
+                {
+                    logo.Begin();
+                }
+            }
+            else
+            {
+                logo.Begin();
+            }
+
+            
         }
-      
+
+        private void Exit() {
+
+            NavigationService.RemoveBackEntry();
+            
+        }
         //當動畫播完後會進入此事件，導向遊戲選單
         private void DoubleAnimationUsingKeyFrames_Completed(object sender, EventArgs e)
         {
-            NavigationService.Navigate(new Uri("/GamePage.xaml", UriKind.Relative));
+            Exit();
+            //NavigationService.Navigate(new Uri("/GamePage.xaml", UriKind.Relative));
         }
     }
 }
